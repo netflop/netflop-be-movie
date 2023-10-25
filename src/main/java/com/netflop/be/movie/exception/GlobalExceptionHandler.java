a7148f1519ad8ccb3e51c7fbc662fb08dbe5cba5
Yen Nguyen <nguyentruongyen1998@gmail.com>
2023-10-25 10:21:16 +0700

    NF-3: Remove redundant code

--- src/main/java/com/netflop/be/movie/exception/GlobalExceptionHandler.java
+++ src/main/java/com/netflop/be/movie/exception/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.netflop.be.movie.exception;
 
-import com.netflop.be.movie.model.BaseResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
